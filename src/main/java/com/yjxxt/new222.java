5e064a8cdf2742b6be3288437d6491ae34ee2ee6
xiongleping <290858971@qq.com>
2022-08-18 16:19:02 +0800

    我

--- src/main/java/com/yjxxt/new222.java
+++ src/main/java/com/yjxxt/new222.java
@@ -4,6 +4,10 @@ public class new222 {
     public static void main(String[] args) {
         System.out.println("new222");
         System.out.println("new222");
+        System.out.println("new222");
+        System.out.println("new222");
+        System.out.println("new222");
+        System.out.println("new222");
 
     }
 }
