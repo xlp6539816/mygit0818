78642a37d946b21627a40ff17450cd5db7bc472a
xiongleping <290858971@qq.com>
2022-08-18 16:13:54 +0800

    主干修改

--- src/main/java/com/yjxxt/new222.java
+++ src/main/java/com/yjxxt/new222.java
@@ -3,5 +3,7 @@ package com.yjxxt;
 public class new222 {
     public static void main(String[] args) {
         System.out.println("new222");
+        System.out.println("new222");
+        System.out.println("new222");
     }
 }
