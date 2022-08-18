3b389daf7f4c04a43b141e5e2cdf709eb179670a
xiongleping <290858971@qq.com>
2022-08-18 15:51:14 +0800

    我修改了输出语句

--- src/main/java/com/yjxxt/Crm.java
+++ src/main/java/com/yjxxt/Crm.java
@@ -2,25 +2,16 @@ package com.yjxxt;
 
 public class Crm {
     public static void main(String[] args) {
-        System.out.println("hello");
-        System.out.println("hellhgjgo");
-        System.out.println("heghjgjhllo");
-        System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
+        System.out.println("当今法国不可见的发表观点看不惯");
+        System.out.println("当今法国不可见的发表观点看不惯");
+        System.out.println("当今法国不可见的发表观点看不惯");
+        System.out.println("当今法国不可见的发表观点看不惯");
+        System.out.println("当今法国不可见的发表观点看不惯");
+        System.out.println("当今法国不可见的发表观点看不惯");
+        System.out.println("当今法国不可见的发表观点看不惯");
+        System.out.println("当今法国不可见的发表观点看不惯");
+        System.out.println("当今法国不可见的发表观点看不惯");
+
 
     }
 }
