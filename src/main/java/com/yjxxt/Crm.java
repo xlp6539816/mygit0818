cb976b3e3bba5daa01154f5e0305fdeb1337152b
xiongleping <290858971@qq.com>
2022-08-18 15:25:48 +0800

    修改crm

--- src/main/java/com/yjxxt/Crm.java
+++ src/main/java/com/yjxxt/Crm.java
@@ -3,5 +3,7 @@ package com.yjxxt;
 public class Crm {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hellhgjgo");
+        System.out.println("heghjgjhllo");
     }
 }
