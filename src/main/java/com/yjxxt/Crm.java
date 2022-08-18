a169dd460ed033016510c48b4808244c6e267f3c
xiongleping <290858971@qq.com>
2022-08-18 15:53:26 +0800

    我也也修改了

--- src/main/java/com/yjxxt/Crm.java
+++ src/main/java/com/yjxxt/Crm.java
@@ -2,6 +2,17 @@ package com.yjxxt;
 
 public class Crm {
     public static void main(String[] args) {
+        System.out.println("观点看不惯");
+        System.out.println("观点看不惯");
+        System.out.println("观点看不惯");
+        System.out.println("观点看不惯");
+        System.out.println("观点看不惯");
+        System.out.println("观点看不惯");
+        System.out.println("观点看不惯");
+        System.out.println("观点看不惯");
+        System.out.println("观点看不惯");
+        System.out.println("观点看不惯");
+        System.out.println("观点看不惯");
         System.out.println("当今法国不可见的发表观点看不惯");
         System.out.println("当今法国不可见的发表观点看不惯");
         System.out.println("当今法国不可见的发表观点看不惯");
