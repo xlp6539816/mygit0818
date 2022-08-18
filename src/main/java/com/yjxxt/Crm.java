70a53786b2d7df400ab5402736e9db6fb9c2a112
xiongleping <290858971@qq.com>
2022-08-18 15:58:42 +0800

    2修改

--- src/main/java/com/yjxxt/Crm.java
+++ src/main/java/com/yjxxt/Crm.java
@@ -4,24 +4,7 @@ public class Crm {
     public static void main(String[] args) {
         System.out.println("观点看不惯");
         System.out.println("观点看不惯");
-        System.out.println("观点看不惯");
-        System.out.println("观点看不惯");
-        System.out.println("观点看不惯");
-        System.out.println("观点看不惯");
-        System.out.println("观点看不惯");
-        System.out.println("观点看不惯");
-        System.out.println("观点看不惯");
-        System.out.println("观点看不惯");
-        System.out.println("观点看不惯");
-        System.out.println("当今法国不可见的发表观点看不惯");
-        System.out.println("当今法国不可见的发表观点看不惯");
-        System.out.println("当今法国不可见的发表观点看不惯");
-        System.out.println("当今法国不可见的发表观点看不惯");
-        System.out.println("当今法国不可见的发表观点看不惯");
-        System.out.println("当今法国不可见的发表观点看不惯");
-        System.out.println("当今法国不可见的发表观点看不惯");
-        System.out.println("当今法国不可见的发表观点看不惯");
-        System.out.println("当今法国不可见的发表观点看不惯");
+
 
 
     }
